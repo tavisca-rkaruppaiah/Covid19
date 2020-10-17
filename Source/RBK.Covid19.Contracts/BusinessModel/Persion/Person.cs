336e8f9111fa1b55819826e3109f2806336c421d
Ragu Balagi Karuppaiah <rkaruppaiah@tavisca.in>
2020-10-17 14:12:25 +0530

    Add project files.

---
+++ Source/RBK.Covid19.Contracts/BusinessModel/Persion/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace RBK.Covid19.Contracts.BusinessModel.Persion
+{
+    public class Person
+    {
+        public Name Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Name
+    {
+        public string First { get; set; }
+        public string Middle { get; set; }
+        public string Last { get; set; }
+        public string Suffix { get; set; }
+        public Title Title { get; set; }
+    }
+
+    public enum Title
+    {
+        Mr,
+        Ms,
+        Mrs,
+    }
+}
